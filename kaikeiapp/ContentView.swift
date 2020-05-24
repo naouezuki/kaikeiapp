0a244bd0c389522fee7b1fc000f4b81558366a88
naouezuki <nao6up@gmail.com>
2020-05-24 19:24:18 +0900

    Initial Commit

---
+++ kaikeiapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kaikeiapp
+//
+//  Created by 上月　菜央 on 2020/05/24.
+//  Copyright © 2020 Nao Uezuki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
