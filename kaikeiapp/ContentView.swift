3008e0cdd249b2c44acd54930ef56c3fc7fb8af7
naouezuki <nao6up@gmail.com>
2020-05-27 21:25:17 +0900

    textfieldの後ろに"円"

--- kaikeiapp/ContentView.swift
+++ kaikeiapp/ContentView.swift
@@ -14,30 +14,31 @@ struct ContentView: View {
     let tax:Double = 1.1
     
     var body: some View {
-        VStack (alignment: .leading){
+//        VStack (alignment: .leading){
             HStack {
                 Text("給料：").padding(.horizontal, 0)
                 TextField("0", text: $salary)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .keyboardType(.numberPad)
-                    .frame(width: 150)
+                    .frame(width: 100)
+                Text("円")
             }
             .font(.title)
             .frame(width: 250)
             
-            Group {
-                salaryCheck {
-                    Text("\(price())円")
-                        .font(.title)
-                }
-            }.frame(width:300, height: 30)
-        }
+//            Group {
+//                salaryCheck {
+//                    Text("\(price())円")
+//                        .font(.title)
+//                }
+//            }.frame(width:300, height: 30)
+//        }
         .position(x:200, y:200)
     }
     
-    func salaryCheck(min:Int, max:Int) -> Bool {
-        _ = Int(salary)
-    }
+//    func salaryCheck(min:Int, max:Int) -> Bool {
+//        _ = Int(salary)
+//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -45,3 +46,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
