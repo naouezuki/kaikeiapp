9ff3eb56dfa2be2237db70819694504d58d41dba
naouezuki <nao6up@gmail.com>
2020-06-14 22:08:51 +0900

    定義づけ１

--- kaikeiapp/ContentView.swift
+++ kaikeiapp/ContentView.swift
@@ -16,63 +16,62 @@ func endEditing(){
         )
     }
 }
-    
 
 struct ContentView: View {
-    @State var salary:String = ""
-    let tanka:Double = 250
-//    let tax:Double = 1.1
-    
-    var body: some View {
-        ZStack {
-            Color.white
-                .onTapGesture {
-                    UIApplication.shared.endEditing()
-                }
-        
-        VStack (alignment: .leading){
-            HStack {
-                Text("給料：").padding(.horizontal, 0)
-                TextField("0", text: $salary)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .keyboardType(.numberPad)
-                    .frame(width: 100)
-                Text("円")
-            }
-            .font(.title)
-            .frame(width: 250)
-                
-            Group{
-                if salaryCheck(min: 1, max: 1000000) {
-//                Text("\(price())円です。")
-                    .font(.title)
-                } else {
-                    Text("金額は正しい金額を入れてください")
-                        .foregroundColor(.red)
-                        .font(.headline)
-                }
-            }.frame(width: 300, height: 30)
-        }
-//        .position(x:200, y:200)
-    }
-}
+    @State var salary:Int
+    let kotei:Double
+    let chokin:Double
+    let special:Double
 
-func salaryCheck(min:Int, max:Int) -> Bool {
-    guard let num = Int(salary) else {
-        return false
-    }
-    return (num>=min && num<=max)
-}
+//    var body: some View {
+//        ZStack {
+//            Color.white
+//                .onTapGesture {
+//                    UIApplication.shared.endEditing()
+//                }
+//
+//        VStack (alignment: .leading){
+//            HStack {
+//                Text("給料：").padding(.horizontal, 0)
+//                TextField("0", text: $salary)
+//                    .textFieldStyle(RoundedBorderTextFieldStyle())
+//                    .keyboardType(.numberPad)
+//                    .frame(width: 100)
+//                Text("円")
+//            }
+//            .font(.title)
+//            .frame(width: 250)
+//
+//            Group{
+//                if salaryCheck(min: 1, max: 1000000) {
+//                Text("\(price())円です。")
+//                    .font(.title)
+//                } else {
+//                    Text("金額は正しい金額を入れてください")
+//                        .foregroundColor(.red)
+//                        .font(.headline)
+//                }
+//            }
+//        }
+//        .position(x:200, y:200)
+//    }
+//}
+//
+//func salaryCheck(min:Int, max:Int) -> Bool {
+//    guard let num = Int(salary) else {
+//        return false
+//    }
+//    return (num>=min && num<=max)
+//}
 //
 //func price() -> Int {
 //    if let num = Double(salary) {
-//        let result = Int(
+//        let result = Int()
 //    }
-}
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
-
